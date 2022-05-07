0b034a53c92341d9e4389bee07451bdeaf6e294d
Jonathan Benson <jbensonamdg@gmail.com>
2022-05-07 17:36:26 -0500

    Implemented working HttpRequest class

--- app/src/main/java/com/example/memestream/MainActivity.java
+++ app/src/main/java/com/example/memestream/MainActivity.java
@@ -4,28 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String serverBase = "http://10.0.2.2:3000/";
+    public static String sessionKey = "";
+    public static String username = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+
         // Set the login onClick listener
         Button loginButton = this.findViewById(R.id.loginButton);
-
-
 
         loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "login", Toast.LENGTH_SHORT).show();
+
+                String response = HttpRequest.executeGet(MainActivity.serverBase);
+
+                Log.d("something", response);
+
+                Toast.makeText(MainActivity.this, "adsf", Toast.LENGTH_SHORT).show();
             }
 
         });
