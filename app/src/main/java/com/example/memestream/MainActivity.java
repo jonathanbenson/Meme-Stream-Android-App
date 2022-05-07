ec812e836555f3102ecb8e2fc95248afba56a1ff
Jonathan Benson <jbensonamdg@gmail.com>
2022-05-07 16:16:42 -0500

    Setup login and register button event listeners in MainActivity

--- app/src/main/java/com/example/memestream/MainActivity.java
+++ app/src/main/java/com/example/memestream/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.memestream;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set the login onClick listener
+        Button loginButton = this.findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "login", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        // Set the register onClick listener
+        Button registerButton = this.findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "register", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
     }
 }
