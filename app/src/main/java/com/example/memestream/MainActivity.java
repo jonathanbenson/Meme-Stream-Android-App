0a35bdf14319be6f2f5f0504b77d19dca54fc394
Jonathan Benson <jbensonamdg@gmail.com>
2022-05-07 20:13:17 -0500

    Implemented login functionality in MainActivity

--- app/src/main/java/com/example/memestream/MainActivity.java
+++ app/src/main/java/com/example/memestream/MainActivity.java
@@ -11,7 +11,11 @@ import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,11 +37,7 @@ public class MainActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View view) {
-
-                new LoginTask().execute(mainActivity);
-
-            }
+            public void onClick(View view) { mainActivity.login(); }
 
         });
 
@@ -55,24 +55,49 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void login() {
+
+        String username = ((EditText)findViewById(R.id.loginUsernameField)).getText().toString();
+        String password = ((EditText)findViewById(R.id.loginPasswordField)).getText().toString();
+
+        if (username.isEmpty() || password.isEmpty())
+            Toast.makeText(this, "Username and password fields cannot be left blank!", Toast.LENGTH_SHORT).show();
+        else
+            new LoginTask().execute(this, username, password);
+    }
+
     public void register() {
         // Goes to the RegisterActivity so the user can register an account
 
         Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
         MainActivity.this.startActivity(intent);
     }
+
+    public void navigateToFeed() {
+        // Navigate back to the FeedActivity
+        Intent intent = new Intent(MainActivity.this, FeedActivity.class);
+        MainActivity.this.startActivity(intent);
+    }
 }
 
-class LoginTask extends AsyncTask<MainActivity, Void, String> {
+class LoginTask extends AsyncTask<Object, Void, String> {
 
     private MainActivity mainActivity;
 
+    private String username;
+    private String sessionKey;
+
     @Override
-    protected String doInBackground(MainActivity... mainActivity) {
+    protected String doInBackground(Object... params) {
 
-        this.mainActivity = mainActivity[0];
+        this.mainActivity = (MainActivity)params[0];
 
-        String response = HttpRequest.executeGet(MainActivity.serverBase);
+        this.username = (String)params[1];
+        String password = (String)params[2];
+
+        String query = MainActivity.serverBase + "login/" + this.username + "/" + password;
+
+        String response = HttpRequest.executeGet(query);
 
         return response;
     }
@@ -80,8 +105,30 @@ class LoginTask extends AsyncTask<MainActivity, Void, String> {
 
     @Override
     protected void onPostExecute(String result) {
-        super.onPostExecute(result);
+        try {
 
-        Toast.makeText(this.mainActivity, result, Toast.LENGTH_SHORT).show();
+            final JSONObject json = new JSONObject(result);
+
+            int status = json.getInt("status");
+
+            if (status == 0)
+                throw new Exception();
+
+            String sKey = json.getString("key");
+
+            MainActivity.sessionKey = sKey;
+            MainActivity.username = this.username;
+
+            Toast.makeText(this.mainActivity, "Welcome back, " + this.username + "!", Toast.LENGTH_SHORT).show();
+
+            this.mainActivity.navigateToFeed();
+
+        }
+        catch (JSONException exc) {
+            Toast.makeText(this.mainActivity, "Cannot connect to server!", Toast.LENGTH_SHORT).show();
+        }
+        catch (Exception excep) {
+            Toast.makeText(this.mainActivity, "Invalid username or password!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
