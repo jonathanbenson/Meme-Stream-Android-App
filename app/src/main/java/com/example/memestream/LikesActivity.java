c48739f49bd60b41f1e5a55133c83577a8de980f
Jonathan Benson <jbensonamdg@gmail.com>
2022-05-09 04:27:43 -0500

    LikesActivity and RegisterActivity comments

--- app/src/main/java/com/example/memestream/LikesActivity.java
+++ app/src/main/java/com/example/memestream/LikesActivity.java
@@ -14,22 +14,28 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 public class LikesActivity extends AppCompatActivity {
+    /*
+    The LikesActivity houses the usernames of the users who liked a particular post.
 
+    It utilizes a ListView container to display the likes.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_likes);
 
+        // Get the Like objects from the FeedActivity
         ArrayList<Like> likes = (ArrayList<Like>)this.getIntent().getSerializableExtra("likes");
 
+        // Convert the list of Like objects into a list of Strings containing the usernames
         ArrayList<String> items = new ArrayList<String>();
-
         for (int i = 0; i < likes.size(); ++i) {
             Like like = likes.get(i);
 
             items.add(like.getUsername());
         }
 
+        // Create the array adapter to be used to project the likes onto the list view
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.listview, items);
         ListView listView = (ListView)this.findViewById(R.id.likesListView);
         listView.setAdapter(adapter);
@@ -50,6 +56,7 @@ public class LikesActivity extends AppCompatActivity {
     }
 
     public void goBack() {
+        // Navigates back to the FeedActivity
 
         Bundle extras = this.getIntent().getExtras();
         String username = extras.getString("username");
--- app/src/main/java/com/example/memestream/RegisterActivity.java
+++ app/src/main/java/com/example/memestream/RegisterActivity.java
@@ -16,17 +16,19 @@ import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    /*
+    The RegisterActivity allows a user to register an account with the app.
+     */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        RegisterActivity registerActivity = this;
 
         // Set the login onClick listener
         Button registerSubmitButton = this.findViewById(R.id.registerSubmitButton);
-
-        RegisterActivity registerActivity = this;
-
         registerSubmitButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -37,7 +39,6 @@ public class RegisterActivity extends AppCompatActivity {
 
         // Set the cancel onClick listener
         Button registerCancelButton = this.findViewById(R.id.registerCancelButton);
-
         registerCancelButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -56,15 +57,22 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void register() {
+        // Handles an attempt to register an account from the user
 
         String username = ((EditText)findViewById(R.id.registerUsernameField)).getText().toString();
         String password = ((EditText)findViewById(R.id.registerPasswordField)).getText().toString();
         String retypePassword = ((EditText)findViewById(R.id.registerRetypePasswordField)).getText().toString();
 
+        // if any of the fields are left blank
+        // then notify the user
         if (username.isEmpty() || password.isEmpty() || retypePassword.isEmpty())
             Toast.makeText(this, "Fields can't be empty!", Toast.LENGTH_SHORT).show();
+        // if the text in the password field doesn't match the text in the retype password field
+        // then notify the user
         else if (!password.equals(retypePassword))
             Toast.makeText(this, "Passwords don't match!", Toast.LENGTH_SHORT).show();
+        // if the user has entered all the required info
+        // then query the database to register
         else
             new RegisterTask().execute(this, username, password);
 
@@ -82,6 +90,10 @@ public class RegisterActivity extends AppCompatActivity {
 
 class RegisterTask extends AsyncTask<Object, Void, String> {
 
+    /*
+    An asynchronous task to register a new account to the server.
+     */
+
     private RegisterActivity registerActivity;
 
     private String username;
@@ -90,14 +102,15 @@ class RegisterTask extends AsyncTask<Object, Void, String> {
     protected String doInBackground(Object... params) {
 
         this.registerActivity = (RegisterActivity)params[0];
-
         this.username = (String)params[1];
         String password = (String)params[2];
 
+        // Create the query
+        // Uses the /register/:username/:password endpoint on the server
         String query = MainActivity.serverBase + "register/" + this.username + "/" + password;
 
+        // Get the response from the server
         String response = HttpRequest.executeGet(query);
-
         return response;
     }
 
@@ -107,17 +120,20 @@ class RegisterTask extends AsyncTask<Object, Void, String> {
 
         try {
 
+            // Parse the response from the server as JSON
             final JSONObject json = new JSONObject(result);
 
+            // If the status of the response is 0 (fail)
+            // then raise an exception
             int status = json.getInt("status");
-
             if (status == 0)
                 throw new Exception();
 
             String sKey = json.getString("key");
 
+            // At this point the server has successfully registered a new account for the user
+            // Give the user a warm welcome and navigate to the FeedActivity
             Toast.makeText(this.registerActivity, "Welcome, " + this.username + "!", Toast.LENGTH_SHORT).show();
-
             this.registerActivity.navigateToFeed(this.username, sKey);
 
         }
