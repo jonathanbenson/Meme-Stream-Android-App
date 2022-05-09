c7a8084866b2addd29004a2440b795da447ceaf2
Jonathan Benson <jbensonamdg@gmail.com>
2022-05-09 00:15:59 -0500

    Implemented RecyclerView for CommentsActivity

--- app/src/main/java/com/example/memestream/CommentsActivity.java
+++ app/src/main/java/com/example/memestream/CommentsActivity.java
@@ -1,32 +1,49 @@
 package com.example.memestream;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Vector;
 
 public class CommentsActivity extends AppCompatActivity {
 
-    private ArrayList<Comment> comments;
+    MyRecyclerViewAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
 
-        this.comments = (ArrayList)this.getIntent().getSerializableExtra("comments");
+        ArrayList<Comment> comments = (ArrayList)this.getIntent().getSerializableExtra("comments");
 
-        for (int i = 0; i < this.comments.size(); ++i) {
-            Comment c = this.comments.get(i);
+        ArrayList<String> items = new ArrayList<String>();
 
-            Log.d("comment", c.getUsername() + " said, \"" + c.getComment() + "\"");
+        for (int i = 0; i < comments.size(); ++i) {
+            Comment c = comments.get(i);
+
+            String item = c.getUsername() + " said, \"" + c.getComment() + "\"";
+
+            items.add(item);
         }
+
+
+        RecyclerView recyclerView = (RecyclerView)this.findViewById(R.id.commentsRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        this.adapter = new MyRecyclerViewAdapter(this, items);
+        recyclerView.setAdapter(adapter);
 
         CommentsActivity commentsActivity = this;
 
@@ -59,5 +76,70 @@ public class CommentsActivity extends AppCompatActivity {
 
         CommentsActivity.this.startActivity(intent);
 
+    }
+}
+
+class MyRecyclerViewAdapter extends RecyclerView.Adapter<MyRecyclerViewAdapter.ViewHolder> {
+
+    private List<String> mData;
+    private LayoutInflater mInflater;
+    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    MyRecyclerViewAdapter(Context context, List<String> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    // inflates the row layout from xml when needed
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.recyclerview, parent, false);
+        return new ViewHolder(view);
+    }
+
+    // binds the data to the TextView in each row
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        String animal = mData.get(position);
+        holder.myTextView.setText(animal);
+    }
+
+    // total number of rows
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+
+    // stores and recycles views as they are scrolled off screen
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView myTextView;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            myTextView = itemView.findViewById(R.id.recyclerViewItem);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // convenience method for getting data at click position
+    String getItem(int id) {
+        return mData.get(id);
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
     }
 }
